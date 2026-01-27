ff466b88dc294da0186f748436a2cae73d771faa
colowill <willminor64@gmail.com>
2026-01-26 19:38:55 -0500

    init commit (check old_parser)

---
+++ old_parser.py
@@ -0,0 +1,522 @@
+import sys
+import string
+
+"""
+@author: Will Minor
+
+
+Parser developed to validate SMTP commands
+
+SMTP (Simple Mail Transfer Protocol) is a protocol used for sending messages from a mail server or a mail client (i.e. gmail) to another mail server.
+
+Commands are constrained by the following grammar:
+
+<mail-from-cmd> ::= “MAIL” <whitespace> “FROM:” <nullspace> <reverse-path> <nullspace> <CRLF>
+"""
+
+
+"""
+cmd (String): the file or input of SMTP commands to parse and validate
+
+Uncomment (1) to input from a file via command:  'python parse.py < <file_name.txt>'
+Uncomment (2) to input from keyboard
+"""
+
+"""(1)"""
+cmd = sys.stdin.read()
+"""(2)"""
+#cmd = input("Type an SMTP command\n")
+
+
+# Array to hold list of  dec values of special ASCII vals
+ascii_special_arr = [60, 62, 40, 41, 91, 93, 92, 46, 44, 59, 58, 64, 34]
+
+# Index of the current character being evaluated by the parser
+c = 0
+
+# Default status message indicating a parse was completed with no errors
+status_message = "Sender ok"
+
+# Bool flag that raises when an error in the parse is detected
+error_exists = False
+
+
+def curr_char():
+    """
+    Used in conditional statements to check current character
+
+    Returns: 
+        current char being evaluated by parser
+    """
+    if c >= len(cmd):
+        return None
+    return cmd[c]
+
+
+def next_char():
+    """
+    Returns: 
+        char that comes after current char
+    """
+    if c+1 >= len(cmd):
+        return None
+    return cmd[c+1]
+
+
+def consume(num):
+    """
+    Consumes the current char, which incriments index of parser
+    To be used in the case that a character is valid
+
+    Args:
+        num (int): Amount of characters to consume (typically 1)
+    """
+    global c
+    c+=num
+
+
+def error_msg(error_msg):
+    """
+    Sets status message to a specific error message string
+
+    Once it catches one error message, each following error message won't be reported
+    """
+    global error_exists
+    if not error_exists:
+        set_status_msg(error_msg)
+        error_exists = True
+
+    
+def set_status_msg(msg):
+    """
+    Helper function to set the global status_message
+
+    Args:
+        msg (String): Status message to be set
+    """
+    global status_message
+    status_message = msg
+
+
+def print_status_msg():
+    """
+    Helper function that prints global status_message
+    """
+    global status_message
+    print(status_message)
+
+
+def echo_cmd(starting_index):
+    """
+    Prints the current command being evaluated by the parser
+
+    Args:
+        index1 (int): Starting index of the parse (c), should be stored by a temp variable in parse_main()
+    """
+    global c
+    print(cmd[starting_index:c].rstrip('\n'))
+
+
+def skip_to_crlf():
+    """
+    In the case that an error is detected before parse_crlf(), this func is called to skip to <crlf>
+    """
+    global c
+
+    while c < len(cmd) and curr_char() != '\n':
+        c+=1
+
+    if c < len(cmd):
+        c+=1
+
+
+def parse_main():
+    """
+    Main parser that calls other recursive descent functions
+    """
+    while not c >= len(cmd):
+        
+        global error_exists
+
+        error_exists = False
+
+        start_index = c
+
+        parse_mail()
+
+        parse_whitespace()
+
+        parse_from()
+
+        parse_nullspace()
+
+        parse_reverse_path()
+
+        parse_nullspace()
+        
+        """
+        If-else block ensures that multiple commands issued in one newline are flagged as <CRLF> error
+        """
+        if error_exists:
+            skip_to_crlf()
+        else:
+            parse_crlf()
+            if error_exists:
+                skip_to_crlf()
+            error_msg("Sender ok")
+        
+        echo_cmd(start_index)
+
+        print_status_msg()
+
+
+def parse_mail():
+    """
+    Ensures first 4 chars of cmd are 'MAIL' and consumes
+
+    Errors:
+        If start of input is missing 'MAIL' 
+    """
+    if not cmd[:4] == "MAIL":
+        error_msg("ERROR - mail-from-cmd")
+    else:
+        consume(4)
+
+
+def valid_sp(): 
+    """
+    Checks if current char is a tab or a space
+
+    If so, consumes char
+
+    Returns:
+        bool: True if current char is valid space, 
+              False otherwise
+    """
+    return curr_char() == ' ' or curr_char() == '\t'
+
+
+def parse_sp():
+    """
+    Consumes current char if it is a tab or a space
+
+    Returns:
+        bool: True if current char is valid space, 
+              False otherwise
+    """
+    if valid_sp():
+        consume(1)
+        return True
+
+    return False
+
+
+def parse_whitespace():
+    """
+    Calls parse_sp() to check if current character is valid <whitespace>
+    Consumes than recursively calls until no more <whitespace> to consume
+
+    Errors:
+        If current character is not a tab or space
+    """
+    if curr_char() == None:
+        error_msg("ERROR -- whitespace")
+        return
+
+    if not parse_sp():
+        error_msg("ERROR -- whitespace")
+        return
+
+    while valid_sp():
+        parse_sp()
+
+
+def parse_from(): 
+    """
+    Ensures next 5 chars after <whitespace> are 'MAIL' and consumes
+
+    Errors:
+        If start of cmd is missing 'MAIL' 
+    """
+    if not cmd[c:c+5] == "FROM:":
+        error_msg("ERROR -- mail-from-cmd")
+    else:
+        consume(5)
+
+
+def parse_crlf():
+    """
+    Parses <CRLF> indicating the end of an SMTP command line, if valid consumes it
+
+    Errors:
+        If the command line does not terminate with CRLF
+    """
+    if curr_char() != '\n':
+        error_msg("ERROR -- CRLF")
+    else:
+        consume(1)
+
+
+def parse_nullspace():
+    """
+    Essentially the same as parse_whitespace(), but allows null inputs
+    
+    If curr_char() is a <whitespace> calls parse_whitespace() to recursively consume it
+    """
+    while curr_char() == ' ' or curr_char() == '\t':
+        parse_whitespace()
+
+
+def valid_letter(character):
+    """
+    Validates if a char is a lowercase (a-z) or uppercase (A-Z) letter
+
+    Assigns ascii_val to the ASCII decimal of curr char, and checks if in range of a valid letter
+
+    Returns:
+        bool: True if character is a letter
+              False if not
+    """
+    ascii_val = ord(character)
+
+    return ascii_val in range(65,91) or ascii_val in range(97,123)
+
+
+def parse_letter():
+    """
+    Parses <letter> then consumes it
+
+    Returns:
+        bool: True if curr char is a letter
+              False if not
+    """
+    if valid_letter(curr_char()):
+        consume(1)
+        return True
+
+    return False
+
+
+def parse_digit():
+    """
+    Checks if curr char is a <digit>
+
+    Assigns ascii_val to the ASCII decimal of curr char, and checks if in range of decimals (0-9)
+
+    If so, consumes char
+
+    Returns:
+        bool: True if curr char is a <digit>
+              False if not
+    """
+    ascii_val = ord(curr_char())
+
+    if ascii_val in range(48,58):
+        consume(1)
+        return True
+
+    return False
+
+
+def valid_char():
+    """
+    Checks if curr char is a valid <char> (as defined by grammar)
+
+    Assigns ascii_val to the ASCII decimal of curr char, and checks if in range of valid char inputs for strings
+
+    Checks if ascii_val is of the array of disallowed special chars
+
+    Returns:
+        bool: True if curr char is a valid <char>
+              False if not
+    """
+    ascii_val = ord(curr_char())
+    
+    if ascii_val not in range(33, 127):
+        return False
+
+    if ascii_val in ascii_special_arr:
+        return False
+
+    return True
+
+
+def parse_char():
+    """
+    Cheks if char is and consumes it
+
+    Returns:
+        bool: True if char is valid
+              False if not
+    """
+    if valid_char():
+        consume(1)
+        return True
+
+    return False
+
+
+def parse_string():
+    """
+    Recursively parses a <string> that is composed of <char>
+
+    Must have at least one valid <char>
+
+    Returns
+        bool: True if atleast one valid char
+              False if not
+    """
+    if not parse_char():
+        return False
+
+    if valid_char():
+        return parse_string()
+
+    return True
+
+
+def parse_local_part():
+    """
+    Parses <local-part> of <mailbox> grammar
+
+    Ensures there's atleast one <char> in <local-part>
+
+    Returns:
+        bool: Trace of parse_string()
+    """
+    return parse_string()
+
+
+def parse_let_dig():
+    """
+    Parses <let-dig>, allows char to be a <letter> or a <digit>
+
+    Returns:
+        bool: True if char is <let-dig>
+              False if not
+    """
+    return parse_letter() or parse_digit()
+
+
+def parse_let_dig_str():
+    """
+    Recursively parses a string of <let-dig>, requiring at least one <let-dig> char
+
+    Returns:
+        bool: True if atleast one <let-dig>
+              False if not
+    """
+    if not parse_let_dig():
+        return False
+
+    if parse_let_dig():
+        parse_let_dig_str()
+    else:
+        return True
+
+
+def parse_name():
+    """
+    Recursively parses <name> which requires at least one <letter> and at least one <let-dig-str>
+
+    Returns:
+        bool: True if both above conditions are valid
+              False if not
+    """
+    return parse_letter() and parse_let_dig_str()
+
+
+def parse_element():
+    """
+    Parses <element> which can be composed of one <letter> or a <name>
+
+    Returns:
+        bool: True if one of above conditions are valid
+              False if not
+    """
+    global c
+    if parse_letter():
+        c-=1
+        parse_name()
+        return True
+    
+    return False
+
+def parse_domain():
+    """
+    Parses <domain> which can be composed of one element or consecutive elements with '.' between them
+
+    Calls <parse_element> to ensure one valid element, then recusrively calls itself if '.' is detected
+
+    Returns:
+        bool:
+            True is elements are arranged correctly
+            False if not
+    """
+    if not parse_element():
+        return False
+    
+    if curr_char() == '.':
+        consume(1)
+        return parse_domain()
+
+    return True
+
+
+def parse_mailbox():
+    """
+    Parses <mailbox> in the form: <local-part> + '@' + <domain>
+
+    Calls <parse_local_part> to validate the local-part, checks for the '@'
+    separator, then calls <parse_domain> to validate the domain portion.
+
+    Errors:
+        If <local-part> is invalid
+        If '@' separator is missing
+        If <domain> is invalid
+    """ 
+    if not parse_local_part():
+        error_msg("ERROR -- string")
+    
+    if curr_char() == '@':
+        consume(1)
+    else: 
+        error_msg("ERROR -- mailbox")
+    
+    if not parse_domain():
+        error_msg("ERROR -- element")
+
+
+def parse_path():
+    """
+    Parses <path> in the form: '<' +  <mailbox> + '>'
+
+    Ensures the path begins with '<', calls <parse_mailbox> to validate the
+    enclosed mailbox, and ensures the path ends with '>'.
+
+    Errors:
+        If opening '<' is missing
+        If <mailbox> is malformed
+        If closing '>' is missing
+    """
+    if curr_char() != '<':
+        error_msg("ERROR -- path")
+    else:
+        consume(1)
+    
+    parse_mailbox()
+
+    if curr_char() != '>':
+        error_msg("ERROR -- path")
+    else:
+        consume(1)
+
+
+def parse_reverse_path():
+    parse_path()
+
+
+try:
+    parse_main()
+except IndexError:
+    print("ERROR -- msg index")
